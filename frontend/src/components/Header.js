7d0d9d2785cdc1ad06230bd39c01559326e5b301
muralidharanfed <muralidharan.fed@gmail.com>
2025-11-28 18:50:02 +0530

    Removed build folder from git tracking

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,7 +5,7 @@ export default function Header({cartItems}) {
     return <nav className="navbar row">
             <div className="col-12 col-md-3">
                 <div className="navbar-brand">
-                <Link to="/"> <img width="150px" src="/images/logo1.png" /></Link>
+                <Link to="/"> <img width="150px" src="/images/logo3.png" /></Link>
                 </div>
             </div>
 
--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -5,7 +5,8 @@ export default function ProductCard({product}) {
             <div className="card p-3 rounded">
                 <img
                 className="card-img-top mx-auto"
-                src={product.images[0].image}
+                src={process.env.PUBLIC_URL + product.images[0].image}
+  alt={product.name}
                 />
                 <div className="card-body d-flex flex-column">
                 <h5 className="card-title">
